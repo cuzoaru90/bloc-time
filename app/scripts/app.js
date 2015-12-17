78308340c2f781a72ddc3c1adfaadf88a4433c38
cuzoaru90@gmail.com <cuzoaru90@gmail.com>
2015-12-17 07:00:00 -0600

    Added ui-router dependeny and template route to Bloctime

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,13 @@
-var app = angular.module("blocTime", ["firebase"]);
+    var app = angular.module("blocTime", ['firebase', 'ui.router']);
+    var myDataRef = new Firebase('https://shining-torch-8271.firebaseio.com/');
+    
 
-var myDataRef = new Firebase('https://shining-torch-8271.firebaseio.com/');
+    app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');     
+    $stateProvider
+      .state('history', {
+      url: '/',
+      controller: 'HistoryCtrl as history',
+      templateUrl: '/templates/history.html'
+      });
+  });
