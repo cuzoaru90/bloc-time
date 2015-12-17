0d22c7033ecdc0e34389dddb7061e2a0e2a1f205
cuzoaru90@gmail.com <cuzoaru90@gmail.com>
2015-12-17 09:41:03 -0600

    Injected interval service into the task controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,6 @@
     var app = angular.module("blocTime", ['firebase', 'ui.router']);
     var myDataRef = new Firebase('https://shining-torch-8271.firebaseio.com/');
     
-
     app.config(function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');     
     $stateProvider
@@ -10,4 +9,19 @@
       controller: 'HistoryCtrl as history',
       templateUrl: '/templates/history.html'
       });
-  });
+    });
+
+    app.controller('taskCtrl', ['$scope', '$interval', function($scope, $interval) {
+
+        $scope.time = (60*25);
+
+        var stop;
+
+        $scope.countdown = function() {
+          
+          stop = $interval(function() {
+              $scope.time -= 1;
+          }, 1000);
+        };
+
+      }]);
