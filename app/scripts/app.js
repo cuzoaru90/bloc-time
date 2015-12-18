ab7202133255311c4a60b002d056b2d9bcf83fa7
cuzoaru90@gmail.com <cuzoaru90@gmail.com>
2015-12-18 10:20:33 -0600

    Enabled application button to start and reset timer

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,13 +15,42 @@
 
         $scope.time = (60*25);
 
+        $scope.taskName = 'Start';
+
         var stop;
 
         $scope.countdown = function() {
+
+          if ( angular.isDefined(stop) ) return;
           
           stop = $interval(function() {
-              $scope.time -= 1;
+            $scope.time -= 1;
           }, 1000);
+
+          $scope.taskName = 'Reset';
         };
+
+        $scope.stopTime = function() {
+          if (angular.isDefined(stop)) {
+            $interval.cancel(stop);
+            stop = undefined;
+          }
+          $scope.taskName = 'Start';
+        };
+
+        $scope.reset = function() {
+          $scope.stopTime();
+          $scope.time = (60*25);
+        };
+
+        $scope.startReset = function() {
+          if ($scope.taskName == 'Start' ){
+            $scope.countdown();
+          }
+          else{
+            $scope.reset();
+          }
+        };
+
 
       }]);
